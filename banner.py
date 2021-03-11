d9311fcdfee5acd01403c45d1f3f9cc23c8535db
HACK3R-AKR <80332174+HACK3R-AKR@users.noreply.github.com>
2021-03-11 22:13:23 +0530

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,30 @@
+#!/user/bin/python
+
+
+#modules required
+import os
+import time
+import sys
+import random
+os.system("clear")
+
+#colours used
+red = '\033[31m'
+yellow = '\033[93m'
+lgreen = '\033[92m'
+clear = '\033[0m'
+bold = '\033[01m'
+cyan = '\033[96m'
+
+#banner of script
+print (lgreen+"""
+  _____      _                 
+(____ \    | |                
+ _   \ \ _ | | ___   ___  
+| |   | / || |/ _ \ /___)
+| |__/ ( (_| | |_| |___ |
+|_____/ \____|\___/(___/
+                                             
+"""+lgreen)
+
+print (cyan+bold+"━━━━❰ coded by HACK3R AKR ❱━━━━ \n"+clear)
